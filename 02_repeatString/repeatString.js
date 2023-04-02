efcf4b92a29b92ac3201e7955e337dc6571955c8
Zan <zan.montieth@gmail.com>
2023-04-02 01:54:29 -0600

    Complete excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stingIn, numIn) {
+    if(numIn < 0){
+        return "ERROR"
+    }
+    let stringOut = "";
+    for(i=0;i<numIn;i++){
+        stringOut += stingIn;
+    }
+    return stringOut
 };
 
 // Do not edit below this line
