325378a3e1f96a94bacccd99b9664d1c6baf8cf5
Zan <zan.montieth@gmail.com>
2023-04-02 04:18:08 -0600

    Complete excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (typeof num1 != "number" || typeof num2 != "number") {
+    return "ERROR";
+  }
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
+  return Array.from({ length: num2 - num1 + 1 }, (_, i) => num1 + i).reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
 };
 
 // Do not edit below this line
