c09954b6256f5ef274dd2dac4fa106fff978b2fe
Zan <zan.montieth@gmail.com>
2023-04-02 04:47:26 -0600

    Complete excercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arrayIn) {
+  let total = 0;
+  while(arrayIn.length > 0) {
+    total += arrayIn.shift(0);
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (arrayIn) {
+  let total = 1;
+  while(arrayIn.length > 0) {
+    total = total * arrayIn.shift(0);
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num1) {
+  let total = 1;
+  if ((num1 == 0)) {
+    return 1;
+  }
+  for (i = 1; i <= num1; i++) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
