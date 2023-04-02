b99b949729290341c143fcbaf5d18bb59a1d32e1
Zan <zan.montieth@gmail.com>
2023-04-02 05:47:09 -0600

    Complete excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function (lookupTerm) {
+  if (Number(lookupTerm) < 0) {
+    return "OOPS";
+  }
+  let term1 = 1;
+  let term2 = 1;
+  let outputTerm = 1;
+  for (let i = 0; i < Number(lookupTerm); i++) {
+    if (i < 2) {
+    } else if (i == 2) {
+      outputTerm = term1 + term2;
+    } else if (i > 2) {
+      [term1, term2] = [term2, outputTerm];
+      outputTerm = term1 + term2;
+    }
+  }
+  return outputTerm;
 };
+
+// fibonacci(3);
 
 // Do not edit below this line
 module.exports = fibonacci;
