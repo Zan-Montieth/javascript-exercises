45d1306838d34fe82f04b516a38a68bd89e84a68
Zan <zan.montieth@gmail.com>
2023-04-02 03:40:48 -0600

    Complete excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayIn) {
+    let arrayOut = [];
+    for (let i = 1; i < arguments.length; i++) {
+        while(arrayIn.length > 0){
+            if(arrayIn[0] === arguments[i]){
+                arrayIn.shift();
+            }else{
+                arrayOut.push(arrayIn.shift());
+            }
+        }
+        if(i + 1 < arguments.length){
+            arrayIn = arrayOut;
+            arrayOut = [];
+        }
+    }
+    return arrayOut
 };
 
 // Do not edit below this line
