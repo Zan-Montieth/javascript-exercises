aed0c54a4122b27a82ad738bdb71c80ff58d5f6a
Zan <zan.montieth@gmail.com>
2023-04-02 05:15:43 -0600

    Complete excercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (arrayIn) {
+  return (
+    arrayIn.split("").filter(isSpace).toString().toLowerCase() ==
+    arrayIn.split("").filter(isSpace).reverse().toString().toLowerCase()
+  );
 };
+
+function isSpace(charIn) {
+  return charIn.toUpperCase() != charIn.toLowerCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
